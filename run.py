2ee329bc40ee510c0a31d8845f58595bb62b3743
Koekuwa <mitsuno.ryota@f.mbox.nagoya-u.ac.jp>
2017-03-21 14:00:28 +0000

    run.pyにcreate tableを対応

--- run.py
+++ run.py
@@ -3,8 +3,23 @@
 """
 cron起動用スクリプト
 """
-
+from argparse import ArgumentParser
 import puller as pull
+import sys
+
+argparser = ArgumentParser()
+argparser.add_argument('--create','-c',help='create table.',action="store_true")
+args = argparser.parse_args()
+
+
+
+
+
 
 if __name__ == "__main__":
-    pull.PullRate().sendToSQL()
+
+    if args.create:
+        pull.PullRate().createTableSQL()
+        sys.exit()
+    else:
+        pull.PullRate().sendToSQL()
